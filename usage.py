5a0648f6b50fa1619e393536f47f076e117f3817
CNFeffery <fefferypzy@gmail.com>
2024-09-09 11:25:29 +0800

    build: 为package.json中的dash-generate-components命令添加参数--keep-prop-order，以在自动生成的参数说明中保持源码顺序

--- usage.py
+++ usage.py
@@ -5,7 +5,7 @@ from dash.dependencies import Input, Output
 
 app = Dash(__name__)
 
-colors = ['red', 'blue', 'yellow']
+colors = ["red", "blue", "yellow"]
 
 app.layout = html.Div(
     [
@@ -13,8 +13,8 @@ app.layout = html.Div(
             [
                 # 热力图层测试
                 fm.HeatmapLayer(
-                    data='https://raw.githubusercontent.com/visgl/deck.gl-data/master/website/sf-bike-parking.json',
-                    getPosition='COORDINATES',
+                    data="https://raw.githubusercontent.com/visgl/deck.gl-data/master/website/sf-bike-parking.json",
+                    getPosition="COORDINATES",
                     radiusPixels=25,
                     colorRange=[
                         [24, 144, 255],
@@ -22,117 +22,89 @@ app.layout = html.Div(
                         [105, 192, 255],
                         [145, 213, 255],
                         [186, 231, 255],
-                        [230, 247, 255]
+                        [230, 247, 255],
                     ],
-                    debounceTimeout=100
+                    debounceTimeout=100,
                 ),
-
                 # 图层排序操作
-                fm.Fragment(
-                    id='action-target'
-                ),
-
+                fm.Fragment(id="action-target"),
                 fm.SourceGroup(
                     [
                         fm.Source(
                             fm.Layer(
-                                id=f'demo-layer{i+1}',
+                                id=f"demo-layer{i+1}",
                                 layerProps={
-                                    'type': 'fill',
-                                    'paint': {
-                                        'fill-color': colors[i]
-                                    }
-                                }
+                                    "type": "fill",
+                                    "paint": {"fill-color": colors[i]},
+                                },
                             ),
-                            id=f'demo-source{i+1}',
+                            id=f"demo-source{i+1}",
                             sourceProps={
-                                'type': 'geojson',
+                                "type": "geojson",
                                 # 模拟生成面要素
-                                'data': {
-                                        'type': 'FeatureCollection',
-                                        'features': [
-                                            {
-                                                'type': 'Feature',
-                                                'geometry': {
-                                                    'type': 'Polygon',
-                                                    'coordinates': [
-                                                        [
-                                                            [-4+i*3, -2+i],
-                                                            [-4+i*3, 2+i],
-                                                            [0+i*3, 2+i],
-                                                            [0+i*3, -2+i],
-                                                            [-4+i*3, -2+i],
-                                                        ]
+                                "data": {
+                                    "type": "FeatureCollection",
+                                    "features": [
+                                        {
+                                            "type": "Feature",
+                                            "geometry": {
+                                                "type": "Polygon",
+                                                "coordinates": [
+                                                    [
+                                                        [-4 + i * 3, -2 + i],
+                                                        [-4 + i * 3, 2 + i],
+                                                        [0 + i * 3, 2 + i],
+                                                        [0 + i * 3, -2 + i],
+                                                        [-4 + i * 3, -2 + i],
                                                     ]
-                                                }
-                                            }
-                                        ]
-                                }
-                            }
+                                                ],
+                                            },
+                                        }
+                                    ],
+                                },
+                            },
                         )
                         for i in range(3)
                     ]
-                )
+                ),
             ],
-            initialViewState={
-                'zoom': 5
-            },
-            style={
-                'height': '100%'
-            }
+            initialViewState={"zoom": 5},
+            style={"height": "100%"},
         ),
-
         html.Div(
-            [
-                html.Button(
-                    '翻转图层顺序',
-                    id='reverse-layers-order'
-                )
-            ],
+            [html.Button("翻转图层顺序", id="reverse-layers-order")],
             style={
-                'background': 'white',
-                'padding': '24px',
-                'borderRadius': 6,
-                'position': 'absolute',
-                'zIndex': 9,
-                'top': 12,
-                'right': 12,
-                'border': '1px solid #bfbfbf',
-                'width': 300,
-            }
-        )
+                "background": "white",
+                "padding": "24px",
+                "borderRadius": 6,
+                "position": "absolute",
+                "zIndex": 9,
+                "top": 12,
+                "right": 12,
+                "border": "1px solid #bfbfbf",
+                "width": 300,
+            },
+        ),
     ],
-    style={
-        'height': '100vh',
-        'position': 'relative'
-    }
+    style={"height": "100vh", "position": "relative"},
 )
 
 
 @app.callback(
-    Output('action-target', 'children'),
-    Input('reverse-layers-order', 'n_clicks'),
-    prevent_initial_call=True
+    Output("action-target", "children"),
+    Input("reverse-layers-order", "n_clicks"),
+    prevent_initial_call=True,
 )
 def handle_layers_order_update(n_clicks):
-
-    if dash.ctx.triggered_id == 'reverse-layers-order':
+    if dash.ctx.triggered_id == "reverse-layers-order":
         return fm.SortLayers(
             orders=(
-                [
-                    'demo-layer3',
-                    'demo-layer2',
-                    'demo-layer1'
-                ]
-                if n_clicks % 2 else
-                [
-                    'demo-layer1',
-                    'demo-layer2',
-                    'demo-layer3'
-                ]
+                ["demo-layer3", "demo-layer2", "demo-layer1"]
+                if n_clicks % 2
+                else ["demo-layer1", "demo-layer2", "demo-layer3"]
             )
         )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run_server(debug=True)
