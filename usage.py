f09ebbe0dab28ee0e10b8f2d92cec5f194fbfa26
CNFeffery <fefferypzy@gmail.com>
2025-03-25 14:38:01 +0800

    chore&feat: 更新底层依赖的maplibre、react-map-gl版本

--- usage.py
+++ usage.py
@@ -1,110 +1,54 @@
 import dash
-from dash import Dash, html
+from dash import html
 import feffery_maplibre as fm
-from dash.dependencies import Input, Output
+from feffery_dash_utils.style_utils import style
 
-app = Dash(__name__, suppress_callback_exceptions=True)
-
-colors = ["red", "blue", "yellow"]
+app = dash.Dash(__name__)
 
 app.layout = html.Div(
     [
         fm.MapContainer(
-            [
-                # 热力图层测试
-                fm.HeatmapLayer(
-                    data="https://raw.githubusercontent.com/visgl/deck.gl-data/master/website/sf-bike-parking.json",
-                    getPosition="COORDINATES",
-                    radiusPixels=25,
-                    colorRange=[
-                        [24, 144, 255],
-                        [64, 169, 255],
-                        [105, 192, 255],
-                        [145, 213, 255],
-                        [186, 231, 255],
-                        [230, 247, 255],
-                    ],
-                    debounceTimeout=100,
-                ),
-                # 图层排序操作
-                fm.Fragment(id="action-target"),
-                fm.SourceGroup(
-                    [
-                        fm.Source(
-                            fm.Layer(
-                                id=f"demo-layer{i + 1}",
-                                layerProps={
-                                    "type": "fill",
-                                    "paint": {"fill-color": colors[i]},
-                                },
-                            ),
-                            id=f"demo-source{i + 1}",
-                            sourceProps={
-                                "type": "geojson",
-                                # 模拟生成面要素
-                                "data": {
-                                    "type": "FeatureCollection",
-                                    "features": [
-                                        {
-                                            "type": "Feature",
-                                            "geometry": {
-                                                "type": "Polygon",
-                                                "coordinates": [
-                                                    [
-                                                        [-4 + i * 3, -2 + i],
-                                                        [-4 + i * 3, 2 + i],
-                                                        [0 + i * 3, 2 + i],
-                                                        [0 + i * 3, -2 + i],
-                                                        [-4 + i * 3, -2 + i],
-                                                    ]
-                                                ],
-                                            },
-                                        }
-                                    ],
-                                },
-                            },
-                        )
-                        for i in range(3)
-                    ]
-                ),
-            ],
-            initialViewState={"zoom": 5},
-            style={"height": "100%"},
-        ),
-        html.Div(
-            [html.Button("翻转图层顺序", id="reverse-layers-order")],
-            style={
-                "background": "white",
-                "padding": "24px",
-                "borderRadius": 6,
-                "position": "absolute",
-                "zIndex": 9,
-                "top": 12,
-                "right": 12,
-                "border": "1px solid #bfbfbf",
-                "width": 300,
+            initialViewState={
+                "longitude": 106,
+                "latitude": 29,
+                "zoom": 2,
             },
-        ),
-    ],
-    style={"height": "100vh", "position": "relative"},
-)
-
-
-@app.callback(
-    Output("action-target", "children"),
-    Input("reverse-layers-order", "n_clicks"),
-    prevent_initial_call=True,
-)
-def handle_layers_order_update(n_clicks):
-    if dash.ctx.triggered_id == "reverse-layers-order":
-        return fm.SortLayers(
-            orders=(
-                ["demo-layer3", "demo-layer2", "demo-layer1"]
-                if n_clicks % 2
-                else ["demo-layer1", "demo-layer2", "demo-layer3"]
-            )
+            mapStyle={
+                "version": 8,
+                "projection": {"type": "globe"},
+                "sources": {
+                    "satellite": {
+                        "url": "https://api.maptiler.com/tiles/satellite-v2/tiles.json?key=get_your_own_OpIi9ZULNHzrESv6T2vL",
+                        "type": "raster",
+                    },
+                },
+                "layers": [
+                    {
+                        "id": "Satellite",
+                        "type": "raster",
+                        "source": "satellite",
+                    },
+                ],
+                "sky": {
+                    "atmosphere-blend": [
+                        "interpolate",
+                        ["linear"],
+                        ["zoom"],
+                        0,
+                        1,
+                        5,
+                        1,
+                        7,
+                        0,
+                    ]
+                },
+                "light": {"anchor": "map", "position": [1.5, 90, 80]},
+            },
+            style=style(height="100%"),
         )
-
+    ],
+    style=style(height="100vh", background="#000"),
+)
 
 if __name__ == "__main__":
     app.run(debug=True)
